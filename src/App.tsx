a1a0b2a4cec1367d3bc9cd26a5280b22250493a4
Alireza Mansourkhaki <a.mansourkhaki@domil.io>
2024-05-06 13:55:21 +0430

    add <ChakraProvider>

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,3 @@
-import { useEffect, useState } from "react";
-import axios, { AxiosError, CanceledError } from "axios";
-
-interface Todo {
-  userId: number;
-  id: number;
-  title: string;
-  completed: boolean;
-}
-
 function App() {
   return <h1>Hello world</h1>;
 }
